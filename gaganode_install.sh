3fe33520b35654169c027a9f9ab5745e9400ecbe
Abu Taher Siddik <138387257+imabutahersiddik@users.noreply.github.com>
2023-08-17 15:21:22 +0600

    Update gaganode_install.sh

--- gaganode_install.sh
+++ gaganode_install.sh
@@ -2,15 +2,15 @@
 
 cpu_arch="$(uname -m)"
 echo "cpu_arch=$cpu_arch"
-AMD="https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz"
-ARM="https://assets.coreservice.io/public/package/61/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz"
+AMD="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample.tar"
+ARM="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample.tar"
 
 if [ "${cpu_arch}" = "x86_64" ]; then
 DOWNLOADLINK=$AMD
-FILENAME="app-linux-amd64.tar.gz"
+FILENAME="sample.tar"
 elif [[ "${cpu_arch}" = "arm64" ]] || [[ "${cpu_arch}" = "aarch64" ]]; then
 DOWNLOADLINK=$ARM
-FILENAME="app-linux-arm64.tar.gz"
+FILENAME="sample.tar"
 else
 echo "Your CPU type dose not support" 
 exit 1
@@ -18,33 +18,9 @@ fi
 
 echo "DOWNLOADLINK=$DOWNLOADLINK"
 
-if [ -d ./apphub-linux* ]; then
-echo "apphub-linux found" &&
-cd ./apphub-linux* &&
-sudo ./apphub service start &&
-sudo ./apphub status &&
-sudo ./apphub log &&
-sudo ./apps/gaganode/gaganode log &&
-cat ./apps/gaganode/user_conf/default.toml
-else
-echo "apphub-linux NOT found."
-sudo curl -o $FILENAME $DOWNLOADLINK &&
-sudo tar -zxf $FILENAME &&
-sudo rm -f $FILENAME &&
-cd ./apphub-linux* &&
-sudo ./apphub service remove &&
-sudo ./apphub service install
-sudo ./apphub service start
-sudo ./apphub status &&
-sleep 30 && echo ' ____   ___  _   _ _____
-|  _ \ / _ \| \ | | ____|
-| | | | | | |  \| |  _|
-| |_| | |_| | |\  | |___
-|____/ \___/|_| \_|_____|'
-sudo ./apphub status && 
-sudo ./apps/gaganode/gaganode config set --token=dhljworgcghoudjl6f2132e015296f50 &&
-sudo ./apphub restart &&
-sudo ./apps/gaganode/gaganode log
-fi
+sudo git clone https://github.com/imabutahersiddik/gaganode.git &&
+cd gaganode &&
+sudo chmod +x gaganode &&
+sudo ./gaganode 
 
 /bin/bash
